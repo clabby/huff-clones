3229704093635dda259f75c888a3e7adc6c144df
clabby <ben@clab.by>
2022-07-04 15:27:12 -0400

    Initial impl

--- test/HuffCloneFactory.t.sol
+++ test/HuffCloneFactory.t.sol
@@ -13,28 +13,6 @@ contract HuffCloneFactoryTest is Test {
         factory = IExampleCloneFactory(
             HuffDeployer.deploy_with_args("ExampleCloneFactory", abi.encode(address(impl)))
         );
-        emit log_named_address("Impl", address(impl));
-        emit log_named_bytes("Impl Code", bytecodeAt(address(impl)));
-        emit log_named_address("Factory", address(factory));
-        emit log_named_bytes("Factory Code", bytecodeAt(address(factory)));
-        emit log_named_bytes32("Impl Slot Factory", vm.load(address(factory), bytes32(0x0)));
-    }
-
-    // https://ethereum.stackexchange.com/questions/66554/is-it-possible-to-get-the-bytecode-of-an-already-deployed-contract-in-solidity
-    function bytecodeAt(address _addr) public view returns (bytes memory code) {
-        assembly {
-            // retrieve the size of the code, this needs assembly
-            let size := extcodesize(_addr)
-            // allocate output byte array - this could also be done without assembly
-            // by using code = new bytes(size)
-            code := mload(0x40)
-            // new "memory end" including padding
-            mstore(0x40, add(code, and(add(add(size, 0x20), 0x1f), not(0x1f))))
-            // store length in memory
-            mstore(code, size)
-            // actually retrieve the code, this needs assembly
-            extcodecopy(_addr, add(code, 0x20), 0, size)
-        }
     }
 
     /// -----------------------------------------------------------------------
